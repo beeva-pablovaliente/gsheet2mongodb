0ef6ae56ef439986ba26f053629387c4b671304b
rekkeb <rekkeb@gmail.com>
2014-08-31 12:12:07 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var gs = require('edit-google-spreadsheet');
+var creds = require('./creds.json');
+
+var fieldNames = ['timestamp', 'username', 'location', 'attendance', 'lastName', 'firstName', 'dob', 'knowledgeLevel', 'knoledgeTecs', 'workExperience', 'emailBeeva', 'emailBbva', 'doi'];
+
+
+gs.load({
+	    debug: true,
+	    spreadsheetId: creds.spreadSheetId, //Nombre de la hoja de calculo
+	    //worksheetId: 'o67dbkn', //Nombre de la 
+	    worksheetName: creds.workSheetName,
+	    // Choose from 1 of the 3 authentication methods:
+	    //    1. Username and Password
+	    //username: 'my-name@google.email.com',
+	    //password: 'my-5uper-t0p-secret-password',
+	    // OR 2. OAuth
+	    //oauth : {
+	    //  email: 'my-name@google.email.com',
+	    //  keyFile: 'my-private-key.pem'
+	    //},
+	    // OR 3. Token
+	    accessToken : {
+	      type: 'Bearer',
+	      token: creds.token
+	    }
+	}, 
+	function sheetReady(err, spreadsheet) {
+	    //use speadsheet!
+	    if (err) { throw err; }
+
+	    //console.dir(spreadsheet.raw);
+
+	    spreadsheet.receive(function(err, rows, info) {
+	        if (err) { throw err; }
+
+	        console.dir(rows);
+	        console.dir(info);
+	    });
+	});
